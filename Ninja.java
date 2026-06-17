d00a9a4bd93257946ec9556ada3c022ea51b334e
Pedro Zagato <117411272+pzgfps@users.noreply.github.com>
2026-06-17 15:50:26 -0300

    Create Ninja.java

---
+++ Ninja.java
@@ -0,0 +1,46 @@
+package NivelIntermediario;
+
+public class Ninja {
+    String nome;
+    String vila;
+    int idade;
+
+    public Ninja(String nome, String vila, int idade) {
+        this.nome = nome;
+        this.vila = vila;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getVila() {
+        return vila;
+    }
+
+    public void setVila(String vila) {
+        this.vila = vila;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "nome = " + nome +
+                ", vila = " + vila +
+                ", idade = " + idade +
+                " \n";
+    }
+}
+
